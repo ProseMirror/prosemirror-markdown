4dea3f44f10d96e63bb4d81af1037ae33037874d
Marijn Haverbeke <marijnh@gmail.com>
2016-01-05 15:15:49 +0100

    Change the module structure for the parsers/serializers (again...)

--- src/inputrules/index.js
+++
@@ -1,7 +0,0 @@
-// !! This module defines a way to attach ‘input rules’ to an editor,
-// which can react to or transform text typed by the user. It also
-// comes with a bunch of default rules that can be enabled with the
-// `autoInput` option.
-
-export {InputRule, defineInputRule, addInputRule, removeInputRule} from "./inputrules"
-import "./autoinput"
