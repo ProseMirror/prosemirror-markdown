6d0c2e5b9322414f99e38e2da8852e74a200bb1a
Erik Moeller <eloquence@gmail.com>
2017-05-10 17:18:19 -0700

    Add default parser tokens to exports
    This makes it easier to modify the default parser behavior

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Defines a parser and serializer for [CommonMark](http://commonmark.org/) text.
 
 exports.schema = require("./schema").schema
-;({defaultMarkdownParser: exports.defaultMarkdownParser, MarkdownParser: exports.MarkdownParser} = require("./from_markdown"))
+;({defaultMarkdownParser: exports.defaultMarkdownParser, defaultMarkdownParserTokens: exports.defaultMarkdownParserTokens, MarkdownParser: exports.MarkdownParser} = require("./from_markdown"))
 ;({MarkdownSerializer: exports.MarkdownSerializer, defaultMarkdownSerializer: exports.defaultMarkdownSerializer,
    MarkdownSerializerState: exports.MarkdownSerializerState} = require("./to_markdown"))
