c0c6fb6849be35307fe980658f0f57189fa5319b
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-05-26 11:05:48 +0100

    Fix indentation

--- src/from_markdown.js
+++ src/from_markdown.js
@@ -242,26 +242,26 @@ export function defaultMarkdownTokens() {
     list_item: {block: "list_item"},
     bullet_list: {block: "bullet_list", getAttrs: (_, tokens, i) => ({tight: listIsTight(tokens, i)})},
     ordered_list: {block: "ordered_list", getAttrs: (tok, tokens, i) => ({
-        order: +tok.attrGet("start") || 1,
-        tight: listIsTight(tokens, i)
-      })},
+      order: +tok.attrGet("start") || 1,
+      tight: listIsTight(tokens, i)
+    })},
     heading: {block: "heading", getAttrs: tok => ({level: +tok.tag.slice(1)})},
     code_block: {block: "code_block", noCloseToken: true},
     fence: {block: "code_block", getAttrs: tok => ({params: tok.info || ""}), noCloseToken: true},
     hr: {node: "horizontal_rule"},
     image: {node: "image", getAttrs: tok => ({
-        src: tok.attrGet("src"),
-        title: tok.attrGet("title") || null,
-        alt: tok.children[0] && tok.children[0].content || null
-      })},
+      src: tok.attrGet("src"),
+      title: tok.attrGet("title") || null,
+      alt: tok.children[0] && tok.children[0].content || null
+    })},
     hardbreak: {node: "hard_break"},
 
     em: {mark: "em"},
     strong: {mark: "strong"},
     link: {mark: "link", getAttrs: tok => ({
-        href: tok.attrGet("href"),
-        title: tok.attrGet("title") || null
-      })},
+      href: tok.attrGet("href"),
+      title: tok.attrGet("title") || null
+    })},
     code_inline: {mark: "code", noCloseToken: true}
   }
 }
